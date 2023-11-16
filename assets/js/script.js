9c682538ef6ff173aa0872cef4e6e899c96d3ece
pcstoyle <pcstoyle@hotmail.com>
2023-11-15 19:54:28 -0700

    got timer working, started on appending questions to html. trouble with buttons - will pick up hee.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,10 @@
-let timeLeft = 90;
-let currentQuestion = 0;
-let userScore = {};
+// let userScore = {};
+var questionIndex = 0;
+let timerInterval;
+var timerEl = document.getElementById("countdown");
+var questionEl = document.getElementById("question");
+var optionsEl = document.getElementById("options");
 
-
-// WHEN I click the start button -- event buttonstener, click start 
-document.getElementById("start-button").addEventListener("click", function() {
-
-});
-
-// THEN a timer starts and I am presented with a question -- timer with a countdown 
-
-
-// WHEN I answer a question -- present question with MC/TF answers, 
-// THEN I am presented with another question  --proceed to next question, create questions attributes, append to DOM, keep count  
 const questions = [{
     question: "Commonly used data types DO NOT include:",
     options: ["Strings", "Booleans", "Alerts", "Numbers",
@@ -44,6 +36,44 @@ const questions = [{
     answer: 1,
 },
 ];
+
+// WHEN I click the start button -- event buttonstener, click start 
+document.getElementById("start-button").addEventListener("click", function startQuiz() {
+    document.getElementById("start").style.visibility = "hidden";
+    document.getElementById("quiz").style.display = "visable";
+    startTimer();
+    displayQuestions();
+});
+// THEN a timer starts and I am presented with a question -- timer with a countdown 
+
+function startTimer() {
+    var time = 60;
+    var timeLeft = setInterval(() => {
+        time--;
+        timerEl.textContent = time;
+    }, 1000);
+};
+
+function displayQuestions() {
+    // pulling from questions object begining with 0 in the array 
+    var currentQuestion = questions[questionIndex];
+
+    questionEl.textContent = currentQuestion.question;
+    optionsEl.textContent = "";
+
+    currentQuestion.options.forEach(option => {
+        const optionsButton = document.createElement("button");
+        optionsButton.textContent = option;
+        optionsEl.appendChild(optionsButton);
+        //     optionsButton.addEventListener("click", () => checkAnswer(option));
+    });
+    questionEl.append(currentQuestion.question);
+        
+    };
+
+// WHEN I answer a question -- present question with MC/TF answers, 
+// THEN I am presented with another question  --proceed to next question, create questions attributes, append to DOM, keep count  
+
 
 
 
